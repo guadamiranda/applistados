fc733a07f12dfccec5781ca0fe8c85616978dba8
Guada <guadamiranda@justiciacordoba.gob.ar>
2023-02-08 13:24:17 -0300

    uwu

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,6 +1,8 @@
 import './footerStyle.css'
+import { GiTwirlyFlower } from 'react-icons/gi'; 
 
 export const Footer = (props) => {
     return  <div className='footerContainer'>
+                <GiTwirlyFlower className='iconFlower'/> Made with a lot of ❤️ by Guadalupe Miranda <GiTwirlyFlower className='iconFlower'/>
             </div>
 } 
