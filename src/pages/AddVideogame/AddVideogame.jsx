dac63f69cadb8cb63d6e43d648d3dc0858d883ca
Guada <guadamiranda@justiciacordoba.gob.ar>
2023-02-09 14:01:31 -0300

    Si

--- src/pages/AddVideogame/AddVideogame.jsx
+++ src/pages/AddVideogame/AddVideogame.jsx
@@ -19,7 +19,7 @@ export const AddOrEditVideogame = () => {
                 </div>
                 <div className='formContainer'>
                     <br/>
-                    <div><VscCircleFilled className='iconColorAddVideogame'/><span>Game name: </span><input type='text' placeholder='Ex. Resident Evil 4'></input></div><br/>
+                    <div><VscCircleFilled className='iconColorAddVideogame'/><span>Name: </span><input type='text' placeholder='Ex. Resident Evil 4'></input></div><br/>
                     <div><VscCircleFilled className='iconColorAddVideogame'/><span>Description: </span><input type='text' placeholder='Ex. Remake'></input></div><br/>
                     <div><VscCircleFilled className='iconColorAddVideogame'/><span>Platform:</span> <input type='text' placeholder='Ex. PC'></input></div><br/>
                     <div><VscCircleFilled className='iconColorAddVideogame'/><span>Inverted hours:</span> <input type='number' placeholder='Ex. 90'></input></div><br/>
