16d50c32641204c6620860c468f0e006e7f0bda5
Guada <guadamiranda@justiciacordoba.gob.ar>
2023-02-10 10:18:40 -0300

    Doing see videogame info

---
+++ src/pages/VideogameInfo/VideogameInfo.jsx
@@ -0,0 +1,17 @@
+import '/videogameInfoStyle.css'
+import '../AllLists/allListsStyle.css'
+import { Header } from '../../components/Header/header';
+import { Footer } from '../../components/Footer/footer';
+
+export const VideogameInfo = (props) => {
+
+    return(
+        <>
+        <Header/>
+        <div className='bodyAppList'>
+        </div>
+        <Footer/>
+        </>
+        
+    )
+}
